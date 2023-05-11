18f9753fc27a8ec358cf889fbb6ca5a69d21137e
imhansai <imhansai@foxmail.com>
2023-05-11 17:11:49 +0800

    Initial commit

--- src/main/java/dev/fromnowon/idea319979/ExportExcelController.java
+++ src/main/java/dev/fromnowon/idea319979/ExportExcelController.java
@@ -1,2 +1,59 @@
-package dev.fromnowon.idea319979;public class ExportExcelController {
+package dev.fromnowon.idea319979;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 导出 Excel 文件
+ *
+ * @author hansai
+ */
+@RestController
+@RequestMapping("exportExcel")
+public class ExportExcelController {
+
+    @GetMapping
+    public void exportExcel(HttpServletResponse response) throws IOException {
+        // 创建工作簿
+        Workbook workbook = new XSSFWorkbook();
+        // 创建工作表
+        Sheet sheet = workbook.createSheet("Sheet1");
+
+        // 创建数据行，并在第一行中写入标题
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("姓名");
+        headerRow.createCell(1).setCellValue("年龄");
+        headerRow.createCell(2).setCellValue("性别");
+
+        // 写入数据行
+        Row dataRow1 = sheet.createRow(1);
+        dataRow1.createCell(0).setCellValue("张三");
+        dataRow1.createCell(1).setCellValue(25);
+        dataRow1.createCell(2).setCellValue("男");
+
+        Row dataRow2 = sheet.createRow(2);
+        dataRow2.createCell(0).setCellValue("李四");
+        dataRow2.createCell(1).setCellValue(30);
+        dataRow2.createCell(2).setCellValue("男");
+
+        // 将数据写入响应流
+        response.setContentType("application/vnd.ms-excel;charset=utf-8");
+        String fileName = URLEncoder.encode("用户-角色", StandardCharsets.UTF_8).replaceAll("\\+", "%20");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+        ServletOutputStream outputStream = response.getOutputStream();
+        workbook.write(outputStream);
+        workbook.close();
+    }
+
 }
--- src/main/java/dev/fromnowon/idea319979/Idea319979Application.java
+++ src/main/java/dev/fromnowon/idea319979/Idea319979Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Idea319979Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Idea319979Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Idea319979Application.class, args);
+    }
 
 }
